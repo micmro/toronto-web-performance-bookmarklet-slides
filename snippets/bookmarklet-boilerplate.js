f4b494227cd176f3ec77ba2b880205188a8532c0
Michael Mrowetz <me@michaelmrowetz.com>
2015-03-09 23:48:10 -0400

    work on resource timing

---
+++ snippets/bookmarklet-boilerplate.js
@@ -0,0 +1,5 @@
+javascript:(function(){
+	var el=document.createElement('script');
+	el.type='text/javascript';el.src='https://nurun.github.io/performance-bookmarklet/dist/performanceBookmarklet.min.js';
+	document.getElementsByTagName('body')[0].appendChild(el);
+})();
